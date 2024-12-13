19c59c880779e1e90f7b2f05016c45fb074b00e0
Saverio Negro <saverionegro@gmail.com>
2024-12-13 23:03:51 +0100

    Initial commit

---
+++ Quotly/QuotlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotlyApp.swift
+//  Quotly
+//
+//  Created by Saverio Negro on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct QuotlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
