58a5ba42da381091616165251b4d363fba1fb1f0
Saverio Negro <saverionegro@gmail.com>
2024-12-14 01:51:41 +0100

    Create `WeekView` to keep track of the weekly quotes brought to life by the user. Keep the user informed about which weekday they are currently on. Allow the user to skim through previous weekdays.

--- Quotly/Views/WeekView.swift
+++ Quotly/Views/WeekView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct WeekView: View {
+    
+    let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+    var currentWeekDay: Int {
+        let date = Date.now
+        let components = Calendar.current.dateComponents([.weekday, .weekdayOrdinal], from: date)
+        let weekday = components.weekday!
+        let index = weekday - 1
+        return index
+    }
+    
+    let rows = [
+        GridItem(.flexible(minimum: 50, maximum: 100))
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        LazyHGrid(rows: rows) {
+            ForEach(weekdays.indices, id: \.self) { index in
+                VStack {
+                    Text(weekdays[index])
+                        .bold(index == currentWeekDay)
+                        .padding(.bottom, 5)
+                    
+                    Text("\(10)")
+                }
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(style: <#T##StrokeStyle#>)
+                }
+            }
+        }
+        .onAppear {
+            let date = Date.now
+            let components = Calendar.current.dateComponents([.weekday, .weekdayOrdinal], from: date)
+            print(components.weekday!)
+        }
     }
 }
 
