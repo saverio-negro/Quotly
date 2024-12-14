50aae9cbdbb36c6bd09e2eac22b55eb6e27e2ab9
Saverio Negro <saverionegro@gmail.com>
2024-12-14 19:08:12 +0100

    Create a color set for the styling  of the app.

---
+++ Quotly/Models/Color-Theme.swift
@@ -0,0 +1,26 @@
+//
+//  Color-Theme.swift
+//  Quotly
+//
+//  Created by Saverio Negro on 14/12/24.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        return Color(red: 0.03, green: 0.06, blue: 0.17)
+    }
+    
+    static var darkBlue: Color {
+        return Color(red: 0.11, green: 0.10, blue: 0.33)
+    }
+    
+    static var aquamarine: Color {
+        return Color(red: 0.33, green: 0.36, blue: 0.57)
+    }
+    
+    static var lightBlue: Color {
+        return Color(red: 0.57, green: 0.56, blue: 0.76)
+    }
+}
