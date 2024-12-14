ad99deb6ce72a18bcfcfec97852062dbad940460
Saverio Negro <saverionegro@gmail.com>
2024-12-14 01:46:55 +0100

    Create `Quote` struct representing each quote.

---
+++ Quotly/Models/Quote.swift
@@ -0,0 +1,11 @@
+//
+//  Quote.swift
+//  Quotly
+//
+//  Created by Saverio Negro on 14/12/24.
+//
+
+struct Quote: Codable {
+    let author: String
+    let text: String
+}
---
+++ Quotly/Views/WeekView.swift
@@ -0,0 +1,18 @@
+//
+//  WeekView.swift
+//  Quotly
+//
+//  Created by Saverio Negro on 14/12/24.
+//
+
+import SwiftUI
+
+struct WeekView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeekView()
+}
